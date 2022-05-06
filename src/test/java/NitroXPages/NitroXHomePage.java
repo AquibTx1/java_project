20f44dd51a9c4ec06d87f32598d50c5daac99892
ArjunDiwan <104342206+ArjunDiwan@users.noreply.github.com>
2022-05-06 13:03:10 +0530

    Locators for Homepage

--- src/test/java/NitroXPages/NitroXHomePage.java
+++ src/test/java/NitroXPages/NitroXHomePage.java
@@ -4,13 +4,19 @@ import org.openqa.selenium.By;
 
 public class NitroXHomePage {
 
-    public static By selectMode=By.xpath("//span[contains(text(),'Spot')]");
+    public static By selectspot=By.xpath("//span[@title='Spot']");
+
+    public static By selectfuture=By.xpath("//div[@title='Futures']/div[1]");
     public static By tradingaccount=By.xpath("//input[@id='account_pair_form_accountId']");
     public static By Basecurrency= By.xpath("//input[@id='account_pair_form_base']");
     public static By Quotecurrency=By.xpath("//input[@id='account_pair_form_quote']");
     public static By price =By.xpath("//input[@id='order_form_price']");
+    //span[contains(text(),'Limit')]
+
     public static By Quantity= By.xpath("//input[@id='order_form_quantity']");
     public static By Dealrefbutton=By.xpath("//input[@id='order_form_deal_ref']");
-    public static By buybtn=By.xpath("");
+    public static By buybtn=By.xpath("//span[contains(text(),'Buy')]");
+    public static By sellbtn= By.xpath("//span[contains(text(),'Sell')]");
+
 
 }
