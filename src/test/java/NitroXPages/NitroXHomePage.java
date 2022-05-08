860545a05581e214200cd761e6b324f9b6875eb3
ArjunDiwan <104342206+ArjunDiwan@users.noreply.github.com>
2022-05-08 22:59:02 +0530

    Locators added

--- src/test/java/NitroXPages/NitroXHomePage.java
+++ src/test/java/NitroXPages/NitroXHomePage.java
@@ -4,17 +4,17 @@ import org.openqa.selenium.By;
 
 public class NitroXHomePage {
 
-    public static By modeTextField = By.id("account_pair_form_mode");
-    public static By spotDropdown = By.xpath("//div[text()='Spot']");
-    public static By getModeTextFieldGeneric = By.xpath("//div[@class='ant-select ant-select-single ant-select-show-arrow ant-select-open ant-select-show-search']/div[@class='ant-select-selector']/span[@Class='ant-select-selection-item']");
-
-    public static By selectfuture = By.xpath("//div[@title='Futures']/div[1]");
-    public static By tradingaccount = By.xpath("//input[@id='account_pair_form_accountId']");
-    public static By Basecurrency = By.xpath("//input[@id='account_pair_form_base']");
-    public static By Quotecurrency = By.xpath("//input[@id='account_pair_form_quote']");
+    public static By modeTextField = By.xpath("(//*[@class='ant-col ant-form-item-control'])[1]/child::div[1]//div[@class='ant-select-selector']");
+    //*[@id='account_pair_form']/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]
+    public static By spotDropdown = By.xpath("(//div[@class='rc-virtual-list-holder-inner'])/div[1]/div");
+    //(//div[@class='ant-select-item-option-content'])[1]
+    public static By FutureDropdown=By.xpath("(//div[@class='rc-virtual-list-holder-inner'])/div[2]/div");
+    public static By getModeTextFieldGeneric = By.xpath("(//*[@class='ant-col ant-form-item-control'])[1]/child::div[1]//div[@class='ant-select-selector']");
+    public static By tradingaccount = By.cssSelector("input[id='account_pair_form_quote']");
+    public static By Basecurrency = By.cssSelector("input[id='account_pair_form_base']");
+    public static By Quotecurrency = By.cssSelector("input[id='account_pair_form_quote']");
     public static By price = By.xpath("//input[@id='order_form_price']");
     //span[contains(text(),'Limit')]
-
     public static By Quantity = By.xpath("//input[@id='order_form_quantity']");
     public static By Dealrefbutton = By.xpath("//input[@id='order_form_deal_ref']");
     public static By buybtn = By.xpath("//span[contains(text(),'Buy')]");
