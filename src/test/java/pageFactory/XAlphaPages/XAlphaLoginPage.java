4ed36f91ca9e99e3524e465aefdc5f453b5b22ca
harj-qa <104417283+harj-qa@users.noreply.github.com>
2022-05-18 10:23:38 +0530

    Created Locators and reusable actions for XAlpha login feature

---
+++ src/test/java/pageFactory/XAlphaPages/XAlphaLoginPage.java
@@ -0,0 +1,12 @@
+package pageFactory.XAlphaPages;
+
+import org.openqa.selenium.By;
+
+public class XAlphaLoginPage {
+
+    public static By username = By.id("login_username");
+    public static By password = By.id("login_password");
+    public static By loginBtn = By.xpath("//button/span[text()='Login']");
+    public static By logoutbtn = By.xpath("//span[text()='Logout']");
+
+}
