ab7cdc7040374d7b24ff5fc18ebfdc6d50c60a51
gouravbhalla@blockchain.com <gouravbhalla@blockchain.com>
2022-05-24 17:41:37 +0800

    adding teamcity scripts

--- .teamcity/scripts/docker-pull.sh
+++ .teamcity/scripts/docker-pull.sh
@@ -3,5 +3,4 @@
 # exit when any command fails
 set -e
 
-
-/usr/bin/curl -X POST https://rundeck.altono.app/rundeck/api/21/job/355fbccf-275d-4c44-bec2-28285d1465a7/run?authtoken=46jwWrWo1AAJp8XyfHJp1jspFqHgSswm
+/usr/bin/curl -X POST https://rundeck.altono.app/rundeck/api/21/job/355fbccf-275d-4c44-bec2-28285d1465a7/run?authtoken=$ALT_REPO_RUNDECK_TOKEN
